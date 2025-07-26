e5c6fb6485bfa9a5555421fe8476339a09f12bbc
Komal <47521309+Komal971@users.noreply.github.com>
2025-07-26 14:19:39 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,117 @@
+//This code is for arithematic expression
+// let a = 12;
+// let b = 20;
+// console.log("Sum of a & b =", a+b);
+
+// let c = "24";
+// console.log("The value of c is", c);
+// console.log("The type of c is", typeof c);
+
+// c = Boolean(c);
+// console.log("The value of c is", c);
+// console.log("The type has change and new type of c is", typeof c);
+
+
+// This code is for if else-if else
+// let marks=60;
+// if (marks>60) {
+//     console.log("Your grade is 'A'");   
+// }else if(marks=60){
+//     console.log("You are doing good in maths.");
+// }else{
+//     console.log("You need to work hard.");
+// }
+// let message = (marks>40)?"You are PASSED!":"You are FAILED!"
+// console.log(message);
+
+
+// This code is foe switch-statement
+// let day=4;
+// let dayName;
+
+// switch (day) {
+//     case 1:
+//         dayName="Monday";
+//         break;
+
+//     case 2:
+//         dayName="Tuesday";
+//         break;
+
+//     case 3:
+//         dayName="Wednesday";
+//         break;
+
+//     default:
+//         dayName="Weekend, enjoy your holiday!"
+//         break;
+// }
+
+// console.log(dayName);
+
+//This section is for loop with break 
+// for (let age = 0; age < 10; age++) {
+//     if (age===5) {
+//         break;
+//     }
+//     console.log("Age:", age);   
+// }
+
+// //This section is for loop with continue 
+// for (let age = 0; age <= 10; age++) {
+//     if (age===4) {
+//         continue;
+//     }
+//     console.log("Age:", age);   
+// }
+
+//this is do-while
+// let i=5;
+// do {
+//     console.log(i);
+//     i++;
+//     if (i===8) {
+//         break;
+//     }
+
+// } while (i<=14);
+
+
+//using clalback function
+// function add(a,b){
+//     console.log("Sum of a & b is", a+b);
+    
+// }
+
+// function sum(callback) {
+//     const a=3;
+//     const b=5;
+//     callback(a,b);
+// }
+
+// sum(add);
+
+//using arrow function
+
+// const a=3;
+// const b=5;
+// const add = (a,b) => {
+//     return a+b;
+// }
+
+// console.log(add(a,b));
+
+// //another way for above arrow function
+
+// const a=3;
+// const b=5;
+// const sum = (a,b) => (a+b) 
+// console.log("sum is :", sum(a,b));
+
+
+// function changeHeading() {
+//     let text = document.getElementById("hey");
+//     text.textContent = "Hello, I am back with new text";
+
+// }
+
